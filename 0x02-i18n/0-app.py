a203786b29e56f41691e9cf6628df17f6891d8a9
Okwor Joshua <105440696+Josh565565@users.noreply.github.com>
2023-08-02 05:34:42 +0100

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
-
 """
-0. Basic Flask app
+Flask app
 """
-
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=["GET"], strict_slashes=False)
-def hello() -> str:
+@app.route('/', strict_slashes=False)
+def index() -> str:
     """
-    hello.
+    Handles / route
     """
     return render_template('0-index.html')
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(port="5000", host="0.0.0.0", debug=True)
